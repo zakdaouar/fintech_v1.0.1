6ed27fe7a92147f483571d95b087b9b3e5ab132f
z.daouar <z.daouar@gmail.com>
2025-08-25 02:07:08 +0200

    back to a working version of backend

--- backend/src/prismaClient.js
+++ backend/src/prismaClient.js
@@ -1,8 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient({
-  datasources: { db: { url: process.env.DATABASE_URL } },
-  log: ["warn","error"],
-});
-
+const prisma = new PrismaClient();
 export default prisma;
